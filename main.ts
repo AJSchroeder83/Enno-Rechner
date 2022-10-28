63291ffeb33741000b584105673e87456d656759
AJSchroeder83 <116520195+AJSchroeder83@users.noreply.github.com>
2022-10-28 19:56:59 +0200

    erste rechenfunktion

--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
 input.onButtonPressed(Button.A, function () {
-    if (0 == 0) {
-    	
-    }
+    Zahl1 = 1125
+    basic.showNumber(Zahl1)
+    basic.pause(3000)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    ergebnis = Zahl1 + zahl2
+    basic.showNumber(ergebnis)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # . # .
+        # # # . #
+        . # # . #
+        . # # . #
+        . # . # .
+        `)
+    basic.pause(3000)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    zahl2 = 2875
+    basic.showNumber(zahl2)
+    basic.pause(3000)
 })
-basic.showString("Ennos gaming")
+let zahl2 = 0
+let ergebnis = 0
+let Zahl1 = 0
+basic.showString("Ennos Rechner")
 basic.forever(function () {
-    music.playMelody("E B C5 A B G A F ", 120)
-    music.playMelody("E B C5 A B G A F ", 120)
+	
 })
