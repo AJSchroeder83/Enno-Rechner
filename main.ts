aea7c5b8121449ab31611b1a4794965df6f8eb09
AJSchroeder83 <116520195+AJSchroeder83@users.noreply.github.com>
2022-10-28 19:08:30 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    if (0 == 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showString("Ennos gaming")
+basic.forever(function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+    music.playMelody("E B C5 A B G A F ", 120)
+})
